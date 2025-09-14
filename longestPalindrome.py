4392531713d8998bd663848511b067a245562bfc
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-13 20:25:32 -0500

    Update longestPalindrome.py

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -10,3 +10,13 @@ class Solution(object):
 
         res = ""
         resLen = 0
+        
+        for i in range(n):
+            # odd cases
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
