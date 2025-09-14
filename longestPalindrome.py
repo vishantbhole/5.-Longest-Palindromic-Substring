1e7f5b374b5483bf72d5d2cc90c50d14078499f2
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-13 20:26:07 -0500

    Update longestPalindrome.py

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -20,3 +20,12 @@ class Solution(object):
                     resLen = r - l + 1
                 l -= 1
                 r += 1
+            # even cases
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        return res
